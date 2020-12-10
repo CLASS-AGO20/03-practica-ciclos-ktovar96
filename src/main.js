a7c1b84f6b9682c3e49f42ab1033a0f7374ae4b3
Karla Tovar <ktovar@ucol.mx>
2020-12-10 16:15:40 -0600

    Implemented Factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-export default class App {}
+export default class App {
+    factorial(numero) {
+        let facto= 1;
+
+        for( let i= 2; i <= numero; i=i+1){
+                facto= facto * i;
+        }
+        return facto;
+    }
+}
 
 let app = new App();
+
+console.log(app.factorial(5));
